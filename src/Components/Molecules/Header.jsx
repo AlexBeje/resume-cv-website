bc892c1a3540658c023611063afe9a948715bf9c
ALEXANDRU BEJENARU <alex.bejenaru93@gmail..com>
2022-03-07 20:18:29 +0100

    👌 IMPROVE: Added the entire GFT experience in spanish.

--- src/Components/Molecules/Header.jsx
+++ src/Components/Molecules/Header.jsx
@@ -6,15 +6,15 @@ import Icon from "/src/Components/Atoms/Icon";
 function Header({ t }) {
   return (
     <>
-      <h1 className={`${globalClasses.center} mt-0 mb-2`}>
+      <h1 className={`${globalClasses.center} m-0`}>
         <span className="font-light text-4xl">
           {t("header.name")} <strong>{t("header.surname")}</strong>
         </span>
       </h1>
-      <code className={`${globalClasses.center} text-xs text-primary`}>
+      <code className={`${globalClasses.center} text-xs text-primary my-2`}>
         {t("header.profession")}
       </code>
-      <p className={`${globalClasses.center} text-xs gap-1 flex-wrap mb-0`}>
+      <p className={`${globalClasses.center} text-xs gap-1 flex-wrap m-0`}>
         <Icon type="phone">{t("header.phone")}</Icon>
         <span>|</span>
         <Icon type="email">{t("header.email")}</Icon>
